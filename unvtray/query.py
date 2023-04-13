a0b00763bf85cc44eaec18e1e31a3adbd67b2d01
Viech <viech@unvanquished.net>
2023-04-13 21:59:16 +0200

    Use a moving average for the servers' ping.
    Implements #6.

--- unvtray/query.py
+++ unvtray/query.py
@@ -63,6 +63,73 @@ def strip_emoticons(string):
     return re.sub("\s*(\[.*?\]|\s)\s*", " ", string).strip()
 
 
+class Ping:
+    def __init__(self, average_over_seconds=60):
+        self._duration = average_over_seconds
+        self._register = []
+        self._min = float("inf")
+
+    def _clear_old(self):
+        if not self._register:
+            return
+
+        num_outdated = 0
+        threshold = unix_time() - self._duration
+        for t, _ in self._register:
+            if t < threshold:
+                num_outdated += 1
+            else:
+                break
+
+        if not num_outdated:
+            return
+        elif num_outdated >= len(self._register):
+            self._register = self._register[-1:]  # Always keep latest.
+        else:
+            self._register = self._register[num_outdated:]
+
+    def __str__(self):
+        if self._register:
+            return f"{self.mvavg * 1000:.0f} ms"
+        else:
+            return "not reached"
+
+    def register(self, response_time_seconds):
+        """Register a current server response time.
+
+        A value of :obj:`None` denotes a failure to respond; this will clear
+        records.
+        """
+        if response_time_seconds is None:
+            self._register.clear()
+        else:
+            t = unix_time()
+            self._register.append((t, response_time_seconds))
+            self._min = min(self._min, response_time_seconds)
+
+    def clear(self):
+        """Clear records."""
+        self.register(None)
+
+    @property
+    def min(self):
+        """Report smallest response time measured in s, or ``float("inf")``."""
+        return self._min
+
+    @property
+    def mvavg(self):
+        """Report average response time over the configured time span in s.
+
+        If the server did not respond to the last request, returns infinity.
+        """
+        self._clear_old()
+
+        if self._register:
+            return sum(r[1] for r in self._register) / len(self._register)
+        else:
+            return float("inf")
+
+
 class Server:
     @staticmethod
     def _valid_or(default=None):
@@ -83,7 +150,7 @@ class Server:
         self._port = port
 
         self._config = None
-        self._ping = None
+        self._ping = Ping()
 
         self._last_refresh = 0
         self._last_refresh_error = None
@@ -121,7 +188,7 @@ class Server:
         t = perf_counter()
         con.send(Q_GET_STATUS)
         response = con.recv(GET_STATUS_BUFFER)
-        self._ping = perf_counter() - t
+        self._ping.register(perf_counter() - t)
 
         if not response:
             raise RuntimeError(
@@ -170,7 +237,7 @@ class Server:
             )
 
             self._config = None
-            self._ping = None
+            self._ping.clear()
 
             self._last_refresh_error = error_str
         else:
@@ -200,10 +267,9 @@ class Server:
         )
 
     @property
-    @_valid_or(float("inf"))
     def ping(self):
-        assert self._ping
-        return self._ping
+        """A moving average of recent response times."""
+        return self._ping.mvavg
 
     @property
     @_valid_or("unknown")
